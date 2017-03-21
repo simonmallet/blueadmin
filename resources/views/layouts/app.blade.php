82acebed9ef45f96e43662334fe4aa06f62307df
Simon Mallet <simonmallet@Simons-MacBook-Pro.local>
2017-03-22 10:50:45 +1300

    added access level in header of dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} <span class="caret"></span>
+                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} ({{ Auth::user()->userPrivilege->level }}) <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
