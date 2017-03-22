dbfca4e3efb167ccea553ad3cf7f2f1148c29264
Simon Mallet <simonmallet@Simons-MacBook-Pro.local>
2017-03-22 18:50:51 +1300

    refactor client view

--- resources/views/client-update.blade.php
+++ resources/views/client-update.blade.php
@@ -5,7 +5,6 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                @if (isset($client->uid))
                 <div class="panel-heading">
                     <h4>{{ $client->name }}</h4>
                 </div>
@@ -23,15 +22,6 @@
                         <button type="submit" class="btn btn-default">Save</button>
                     </form>
                 </div>
-                @else
-                    <div class="panel-heading">
-                        <h4>Whoops!</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>It looks like this client does not exist or you don't have permission to view it.</p>
-                        <a href="/">Go back to dashboard</a>
-                    </div>
-                @endif
             </div>
         </div>
     </div>
